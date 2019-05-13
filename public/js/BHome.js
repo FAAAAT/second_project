b90ea1ff03cbea605676468caed9e7a57acb23c3
gongchangyueban <50438587+gongchangyueban@users.noreply.github.com>
2019-05-13 18:48:15 +0800

    修改主页显示效果
    修改主页显示效果

--- public/js/BHome.js
+++ public/js/BHome.js
@@ -3,37 +3,32 @@ let oBleft=$('#B-left');
 let oBRight=$('#B-right');
 let oBleftName=$('#B-name');
 let oBBoss=$('#B-boss');
-let oBRightLogo=$('B-right-logo');
+let oBRightLogo=$('#B-right-logo');
+
 //当页面加载时获取可视区高度
 window.onload= Bwidth;
 //当页面缩放时
 window.onresize=Bwidth;
 
 function Bwidth() {
-    let oLookW=$(window).width();
-    let oLookH=$(window).height();
-    oBBoss.css('height',oBleft.outerHeight()-oBBoss.offset().top-10);
-    if (oLookW<500){
-        oBleft.css('width',500);
-        oBleftName.css('width',500);
-        oBRight.css('width','480');
-        oBBoss.css('height','600');
-    }else {
-        oBleft.css('width',180);
-        oBleftName.css('width',180);
-        oBRight.css('width',oLookW-oBleft.outerWidth());
-    }
-   /* if (oLookW-oBleft.outerWidth()<=0){
-        oBRight.css('width','500');
-    } else {
-        oBRight.css('width',oLookW-oBleft.outerWidth());
-    }*/
+    let oLookW = $(window).width();
+    let oLookH = $(window).height();
+    let oBExitH=oBRightLogo.outerHeight();
     if (oLookH<630) {
         oBleft.css('height','630');
+        oBRight.css('height','630');
+        oBBoss.css('height',oBRight.outerHeight()-oBExitH-40);
+
     }else {
         oBleft.css('height',oLookH);
-
+        oBRight.css('height',oLookH);
+        oBBoss.css('height',oBRight.outerHeight()-oBExitH-40);
     }
-
-    console.log(oBleft.outerHeight(),oBBoss.offset().top,oLookH)
+    if (oLookW<500){
+        oBleft.css('width','500');
+        oBRight.css('width','500')
+    } else {
+        oBleft.css('width','180');
+        oBRight.css('width',oLookW-182)
+    }
 }
