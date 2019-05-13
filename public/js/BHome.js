1ba97393b37db8e9314c53f34c2bf1de930e3c96
gongchangyueban <50438587+gongchangyueban@users.noreply.github.com>
2019-05-13 16:40:31 +0800

    全面修改主页相应方式
    全面修改主页相应方式

---
+++ public/js/BHome.js
@@ -0,0 +1,36 @@
+let oLook=$(window).height();
+let oBleft=$('#B-left');
+let oBRight=$('#B-right');
+let oBleftName=$('#B-name');
+let oBBoss=$('#B-boss');
+let oBRightLogo=$('B-right-logo');
+//当页面加载时获取可视区高度
+window.onload=function () {
+    if (oLook<630) {
+        oBleft.css('height','630');
+    }else {
+        oBleft.css('height',oLook);
+    }
+    console.log(oBleft.outerHeight(),oBBoss.offset().top);
+    oBBoss.css('height',oBleft.outerHeight()-oBBoss.offset().top-10);
+    Bwidth();
+};
+//当页面缩放时
+window.onresize=Bwidth;
+
+function Bwidth() {
+    let oLookW=$(window).width();
+    if (oLookW<500){
+        oBleft.css('width',500);
+        oBleftName.css('width',500);
+    }else {
+        oBleft.css('width',180);
+        oBleftName.css('width',180);
+    }
+    if (oLookW-oBleft.outerWidth()<0){
+        oBRight.css('width','500');
+    } else {
+        oBRight.css('width',oLookW-oBleft.outerWidth());
+    }
+
+}
