c11fd347a72fc17d4ddefa3336783842fe06d6ca
longduying <50438575+longduying@users.noreply.github.com>
2019-05-14 11:26:20 +0800

    修改
    修改

--- public/js/C-class.js
+++ public/js/C-class.js
@@ -1,56 +1,78 @@
-// 弹框js
-var oMask=$('#mask');
-var oA=$('.close');
-var oSave=$('#save');
-var oCancel=$('#cancel');
-var oAdd=$('.btn2');
-var oEdio=$('.edio');
-var oDel=$('.del');
-var omask=$('.mask');
-var oKeep=$('.keep');
+    // 弹框js
+    var oMask=$('#mask');
+    var oA=$('.close');
+    var oSave=$('#save');
+    var oCancel=$('#cancel');
+    var oAdd=$('.btn2');
+    var oEdio=$('.edio');
+    var oDel=$('.del');
+    var omask=$('.mask');
+    var oKeep=$('.keep');
+    var oAll=$('.all');
 
 
-oSave.click(function () {
-    oKeep.css('display','block')
-})
-oAdd.click(function () {
+    oSave.click(function () {
+        oKeep.css('display','block')
+    });
+    oAdd.click(function () {
+        oAll.css('display','block');
+        oMask.css('display','block');
+    });
+    oA.click(function () {
+        oMask.css('display','none');
+        oAll.css('display','none')
+    });
+    oCancel.click(function () {
+        oMask.css('display','none');
+        oAll.css('display','none')
+    });
+    oDel.click(function () {
+        omask.css('display','block')
+    });
+    // console.log(oEdio);
+    oEdio.click(function () {
+        oAll.css('display','block');
         oMask.css('display','block')
-});
-oA.click(function () {
-    oMask.css('display','none')
-});
-oCancel.click(function () {
-    oMask.css('display','none')
-});
-oDel.click(function () {
-    omask.css('display','block')
-});
-// 删除提示小弹框
-let oDelete=$('#delete');
-let oClose=$('#close');
-oDelete.click(function () {
-    omask.css('display','block')
-});
-let oNo=$('.no');
-oNo.click(function () {
-    omask.css('display','none');
-});
-let oSure=$('#sure');
-oSure.click(function () {
-    omask.css('display','none');
-});
-oNo.click(function () {
-    oKeep.css('display','none')
-});
-let osure=$('.sure')
-osure.click(function () {
-    oKeep.css('display','none')
-})
-let oYes=$('.yes');
-oYes.click(function () {
-    oKeep.css('display','none')
-    oMask.css('display','none')
-})
-oEdio.click(function () {
-    oMask.css('display','block')
-})
+    });
+
+    // 删除提示小弹框
+    let oDelete=$('#delete');
+    let oClose=$('#close');
+    let oNo=$('.no');
+    let oSure=$('#sure');
+    let osure=$('.sure');
+    let oYes=$('.yes');
+    oDelete.click(function () {
+        omask.css('display','block')
+    });
+    oNo.click(function () {
+        omask.css('display','none');
+    });
+    oSure.click(function () {
+        omask.css('display','none');
+    });
+    oNo.click(function () {
+        oKeep.css('display','none')
+    });
+
+    osure.click(function () {
+        oKeep.css('display','none')
+    });
+
+    oYes.click(function () {
+        oKeep.css('display','none');
+        oMask.css('display','none');
+        oAll.css('display','none');
+    });
+
+
+    // 查询功能
+        var oT1=$('.t1');
+        var oBtn1=$('.btn1');
+
+        oBtn1.onclick=function (){
+            var str1=oT1.val()
+            $("#mapAddress tr").hide();
+            $("#mapAddress tr td").filter(":contains('" + str1 + "')").parents("tr").show();
+        }
+
