c8b76e17a886b54a09d1f72b7520767317449af1
zhaohui <1192399115@qq.com>
2019-05-15 18:00:31 +0800

    改正则

--- public/js/D-department.js
+++ public/js/D-department.js
@@ -450,7 +450,7 @@ new Form_4(".D-table").addnew("#test").addcheck({
         {
             func:function (str) {
                 if(str.trim()){
-                    var time = /^\d$/;
+                    var time = /\d{4}-\d{2}-\d{2}/;
                     
                     if(time.test(str)){
                         return true
