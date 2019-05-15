90698489a40e32dba5bf770ad412dd66046667c2
happylee123 <49627751+happylee123@users.noreply.github.com>
2019-05-15 14:14:49 +0800

    L-4
    L-4

--- public/js/L-student.js
+++ public/js/L-student.js
@@ -1,69 +1,17 @@
-    // // 弹框js
-    // var oMask=$('#mask');
-    // var oA=$('.close');
-    // var oSave=$('#save');
-    // var oCancel=$('#cancel');
-    // var oAdd=$('.btn2');
-    // var oEdio=$('.edio');
-    // var oDel=$('.del');
-    // var omask=$('.mask');
-    // var oKeep=$('.keep');
-    // var oAll=$('.all');
-    //
-    //
-    // oSave.click(function () {
-    //     oKeep.css('display','block')
-    // });
-    // oAdd.click(function () {
-    //     oAll.css('display','block');
-    //     oMask.css('display','block');
-    // });
-    // oA.click(function () {
-    //     oMask.css('display','none');
-    //     oAll.css('display','none')
-    // });
-    // oCancel.click(function () {
-    //     oMask.css('display','none');
-    //     oAll.css('display','none')
-    // });
-    // oDel.click(function () {
-    //     omask.css('display','block')
-    // });
-    // // console.log(oEdio);
-    // oEdio.click(function () {
-    //     oAll.css('display','block');
-    //     oMask.css('display','block')
-    // });
-    //
-    // // 删除提示小弹框
-    // let oDelete=$('#delete');
-    // let oClose=$('#close');
-    // let oNo=$('.no');
-    // let oSure=$('#sure');
-    // let osure=$('.sure');
-    // let oYes=$('.yes');
-    // oDelete.click(function () {
-    //     omask.css('display','block')
-    // });
-    // oNo.click(function () {
-    //     omask.css('display','none');
-    // });
-    // oSure.click(function () {
-    //     omask.css('display','none');
-    // });
-    // oNo.click(function () {
-    //     oKeep.css('display','none')
-    // });
-    //
-    // osure.click(function () {
-    //     oKeep.css('display','none')
-    // });
-    //
-    // oYes.click(function () {
-    //     oKeep.css('display','none');
-    //     oMask.css('display','none');
-    //     oAll.css('display','none');
-    // });
+  
+  
+  function type(){
+    var firstinputval=$("#mask input:first").val();
+    var classtype=["大班","小班","中班"];
+    for(var cl=0;cl<classtype.length;cl++){
+        if(firstinputval==classtype[cl]){
+            return true;
+        }
+    }
+  }
+    
+
+
 
 
     // 查询功能
